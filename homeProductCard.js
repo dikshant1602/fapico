5797728ea1a32e968b8cf99a98fda50b3b911463
Dikshant Choudhary <dikshantchoudhary12345@gmail.com>
2025-07-12 00:04:33 +0530

    adding more elements and images and changing ui styles

---
+++ homeProductCard.js
@@ -0,0 +1,26 @@
+const productsContainer = document.querySelector('#productContainer');
+const productTemplate = document.querySelector('#productTemplate');
+
+export const showProductContainer = (products) => {
+  if(!products){
+    return false;
+  }
+
+  products.forEach((currProd) => {
+    const {category, description, id, image, name, price, stock} = currProd;
+
+    const productClone = document.importNode(productTemplate.content, true);
+
+    productClone.querySelector('.productName').textContent = name;
+    productClone.querySelector('.productImage').src = image;
+    productClone.querySelector('.productImage').alt = name;
+    //productClone.querySelector('.productStock').textContent = stock;
+    productClone.querySelector('.productDescription').textContent = description;
+    productClone.querySelector('.category').textContent = category;
+    productClone.querySelector('.productPrice').textContent = `₹${price}`;
+    productClone.querySelector('.productActualPrice').textContent = `₹${price * 2}`;
+
+
+    productsContainer.append(productClone);
+  });
+};
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-import 'src/style.css'
+import '../src/style.css';
+
+import products from '../api/products.json'
+import { showProductContainer } from '../homeProductCard';
+
+// Function to create a product card
+showProductContainer(products);
