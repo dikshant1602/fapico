dda7670d1a059ed9808d3673d9cde703375ca669
Dikshant Choudhary <dikshantchoudhary12345@gmail.com>
2025-07-13 00:58:19 +0530

    ui changes

--- footer.js
+++ footer.js
@@ -11,21 +11,22 @@ const footerHTML = `
         <div class="content_2">
           <h4>Services</h4>
           <a href="#">Pest Control</a>
+          <a href="#">Commercial Pest Control</a>
           <a href="#">Deep Cleaning</a>
+          <a href="#">House Cleaning</a>
         </div>
         <div class="content_3">
           <h4>Experience</h4>
-          <a href="./contact.html">Contact Us</a>
-          <a href="" target="_blank">Adress</a>
-          <a href="" target="_blank">Instagram</a>
+          <a href="./contact.html" target="_blank">Contact Us</a>
+          <a href="https://www.linkedin.com/in/fapico-fapico-613aa3371/?originalSubdomain=in" target="_blank">Linkdin</a>
+          <a href="https://www.instagram.com/fapico_/" target="_blank">Instagram</a>
+          <a href="https://www.facebook.com/people/Fapico/61577772155917/#" target="_blank">Facebook</a>
         </div>
+
         <div class="content_4">
-          <h4>NEWSLETTER</h4>
-          <p>Be the first to know about new<br />arrivals, sales & promos!</p>
-          <div class="f-mail">
-            <input type="email" placeholder="Your Email" />
-            <i class="bx bx-envelope"></i>
-          </div>
+          <h4>E-Mail</h4>
+          <p>Feel free to get in touch with us for<br />services, prices & bookings!</p>
+          <a href=""contact@fapico.com>Email-Us:contact@fapico.com</a>
           <hr />
         </div>
       </div>
